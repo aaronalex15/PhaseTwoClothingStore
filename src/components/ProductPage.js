737b21a35cce4f84eec8806109ece2e90e9393cc
Pedro Alarcon <pedroa54@icloud.com>
2024-04-12 18:28:32 -0500

    adds fixes to routes

---
+++ src/components/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductPage() {
+    return (
+        <div classname="Home">
+            <h1>ProductPAge</h1>
+        </div>
+    )
+
+}
+
+export default ProductPage;
--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Splash() {
     return (
         <>
-        <header>
-            <h1>Shop-EASE</h1>
-        </header>
-        <button>SHOP</button>
+            <header>
+                <h1 >Shop-EASE</h1>
+            </header>
+            <Link to="/SHOP">
+                <button>SHOP</button>
+            </Link>
         </>
     )
 }
-
 
 export default Splash;
