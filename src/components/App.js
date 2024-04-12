5e950643390031e63f43713cd3308df2d2a0102e
aaronalex15 <aaronalex1230@gmail.com>
2024-04-12 16:24:31 -0700

    products on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
-import Splash from "./Splash"
-import ProductPage from "./ProductPage"
-
-function App() {
-return (
-  <div className="App">
-    <Splash />
-    <ProductPage />
-=======
 import Splash from "./Splash";
 import ProductCard from "./ProductCard";
 
 
 function App() {
 return (
-  <div classname="app">
-    <splash />
-    <productcard />
->>>>>>> 7594ddb6ba461f2f4767b301313cbbd0f994bc22
+  <div className="App">
+    <Splash />
+    <ProductCard />
 
 
 
