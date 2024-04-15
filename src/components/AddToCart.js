63e7149fb5d85840ad4c8264ef319e4583d3dd5d
aaronalex15 <aaronalex1230@gmail.com>
2024-04-15 10:38:51 -0700

    css changes to search, add to cart, and the cards

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -2,17 +2,32 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 
-const AddToCart = ({id}) => {
+const AddToCart = ({ id }) => {
     const handleAddToCart = () => {
         console.log('Item added to cart!');
     };
 
     return (
-       <Link to ={`/product/${id}`} >
-        <Button>
-            Add to Cart
-        </Button>
+        <Link to={`/product/${id}`} style={{ textDecoration: 'none' }}>
+            <Button
+                onClick={handleAddToCart}
+                variant="contained" 
+                color="primary" 
+                style={{
+                    backgroundColor: 'red', 
+                    color: 'white', 
+                    padding: '10px 20px', 
+                    borderRadius: '5px', 
+                    fontWeight: 'bold', 
+                    fontSize: '14px', 
+                    marginTop: '10px', 
+                }}
+            >
+                Add to Cart
+            </Button>
         </Link>
     );
 };
+
 export default AddToCart;
+
