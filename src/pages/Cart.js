5ca47db8d5158dd393e97a6281c69c1ca48d6d47
Pedro Alarcon <pedroa54@icloud.com>
2024-04-16 17:38:29 -0500

    organizes files

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,7 +7,7 @@ import '../components/Style.css';
 function Cart() {
     return (
         <div className="cart-container">
-            <h1 className="main-title">SHOP-EASE</h1>
+            <h1 className="main-title">SmoothShop</h1>
             
             <Link to="/shop">
                 <Button className="shop-button">SHOP</Button>
@@ -15,7 +15,7 @@ function Cart() {
             
             <Link to="/">
                 {}
-                <Button className="shop-ease-button">SHOP-EASE</Button>
+                <Button className="shop-ease-button">SmoothShop</Button>
             </Link>
             
             <Container>
