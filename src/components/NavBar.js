e4250725670fe1face39549156d374c3c42c9769
Pedro Alarcon <pedroa54@icloud.com>
2024-04-17 17:55:31 -0500

    adds styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,8 @@
 import { NavLink } from "react-router-dom";
-
+testing 
 function NavBar() {
     return (
     <nav className="navbar">
-    
     <NavLink to="/">Splash</NavLink>
     <NavLink to="/shop">MainMenu</NavLink>
     <NavLink to="/product/:id">Cart</NavLink>
