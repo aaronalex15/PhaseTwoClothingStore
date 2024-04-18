687b7ee5cedb101dbd82f5fa752bf39fc63939e8
Pedro Alarcon <pedroa54@icloud.com>
2024-04-18 11:41:49 -0500

    fix routing

--- src/pages/MainMenu.js
+++ src/pages/MainMenu.js
@@ -1,21 +1,31 @@
-import { Container } from "@mui/material";
+import { Container, Button } from "@mui/material";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import ProductList from "../components/ProductList";
 import Search from "../components/Search";
 import NewProduct from "../components/NewProduct";
 
-function MainMenu() {
+    function MainMenu() {
     const [productList, setProductList] = useState([]);
-
+    
     // Function to add a new product to the product list
     const addProduct = (formData) => {
         // Append the new product to the existing product list
         setProductList([...productList, formData]);
     };
-
+    
     return (
         <div className="Home">
             <h1 className="main-title">SmoothShop</h1>
+            
+            <Link to="/product/:id">
+                <Button className="shop-button">Checkout</Button>
+            </Link>
+            
+            <Link to="/">
+                <Button className="smooth-shop">SmoothShop</Button>
+            </Link>
+            
             <Container>
                 <br />
                 <Search />
